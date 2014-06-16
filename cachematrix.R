77024e554a1ad0a597dbe90e0075c68e50b02854
Brandon Tuomela <brandon@jaanj.com>
2014-06-15 18:34:48 -0700

    memorize the solve function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Memorize the solve() function for matrices by building an object with
+## makeCacheMatrix and using the object with cacheSolve
 
-## Write a short comment describing this function
-
+## return a list of functions to get and set a matrix
+## and 2 functions to get and get an inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Use the list created from makeCacheMatrix and check for a cached value
+## if not found calculate it and set the value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached value")
+        return(inv)
+    }
+    
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
